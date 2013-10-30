908d077d88018137c14526cba3022720c9739992
rolandnsharp <rolandnsharp@gmail.com>
2013-10-30 23:36:39 +1000

    mail2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,23 @@ Arachne::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  #domain: "arachnewriter.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "roland@arachnewriter.com",
+  password: "28trx784wrx",
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
