64461741e3d83f62881ee09f489269ede49f12f3
rolandnsharp <rolandnsharp@gmail.com>
2013-11-14 13:39:55 +1000

    title css

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,6 +1,9 @@
 class Thought < ActiveRecord::Base
   belongs_to :user
 
+  validates :content, presence: true,
+                    uniqueness: { case_sensitive: false }
+
   def self.text_search(query)
   	if query.present?
   		where("content like :q", q: "%#{query}%")
