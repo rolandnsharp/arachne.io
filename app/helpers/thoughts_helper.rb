4b5068767030460c666fa9be7e45a597781cc530
rolandnsharp <rolandnsharp@gmail.com>
2013-11-14 16:30:00 +1000

    nodes now accepting title

--- app/helpers/thoughts_helper.rb
+++ app/helpers/thoughts_helper.rb
@@ -1,2 +1,33 @@
 module ThoughtsHelper
+
+	def web_array_maker(thoughts)
+		
+
+		thought_web_array=[] 
+		 
+
+		thoughts.each do |thought|
+		 	 title_string=  thought.title.to_s
+
+		 	 if title_string.length == 0
+		 	         
+				str=  thought.content.to_s   
+			    tags = str.scan(/#\w+/).uniq 
+				thought_web_array= thought_web_array+[tags] 
+		 	
+		 	 else
+
+		 	 	str=  thought.content.to_s   
+			    tags = str.scan(/#\w+/).uniq 
+			    title_array = tags.unshift(title_string)
+				thought_web_array= thought_web_array+[title_array] 
+
+
+		 	 end
+
+				 
+		end 
+		return thought_web_array
+	end
+	
 end
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,9 +1,10 @@
 class Thought < ActiveRecord::Base
   belongs_to :user
 
-  validates :content, presence: true,
-                    uniqueness: { case_sensitive: false }
-
+  validates :content, presence: true
+            
+  validates_uniqueness_of :title, :allow_blank => true, :case_sensitive => false
+  
   def self.text_search(query)
   	if query.present?
   		where("content like :q", q: "%#{query}%")
