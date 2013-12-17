2c0713e1e0933591e89df5721b0595ccb3593a8d
rolandnsharp <rolandnsharp@gmail.com>
2013-12-17 21:05:51 +1000

    sign up form sigma background

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if @user && @user.authenticate(params[:password])
   		session[:user_id] = @user.id
   		redirect_to @user
-      flash[:success] = "Logged in!"
+      # flash[:success] = "Logged in!"
   	else	
 
   		redirect_to root_url
